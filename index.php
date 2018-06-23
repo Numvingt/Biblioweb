c007ba0504e97ef069d5adba8036bcd245f2a0df
Laurent Nosella <laurentnosella88@gmail.com>
2018-06-23 11:25:13 +0200

    design : wip

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php session_start();?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>BiblioWeb</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <script src="js.js" refer></script>
+</head>
+
+<body>
+
+    <?php include("navbar.php"); ?>
+
+    <div class="boop">
+
+        <div id="time"></div>
+        <div id="greet"></div>
+
+    </div>
+
+</body>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,15 @@
+<nav>
+
+    <label for="show"><span class="hamburg">&#9776;</span></label>
+    <input type="checkbox" id="show">
+
+    <ul class="menu">
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="inscription.php">S'inscrire</a></li>
+        <li><a href="connexion.php">Se connecter</a></li>
+        <li><a href="display.php">Bibliothèque</a></li>
+        <li><a href="addBook.php">Ajouter un livre</a></li>
+        <li><a href="remove.php">Supprimer un livre</a></li>
+    </ul>
+
+</nav>
